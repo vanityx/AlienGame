1e9b69f2ca408c595282b7223988053cf152be15
Vanityx <nothuntr@gmail.com>
2020-07-06 22:18:58 +0100

    Cleaned file

--- assets/main.py
+++ assets/main.py
@@ -36,56 +36,6 @@ slime = Slime(0, 480)
 # scoreboard
 scoreboard = Scoreboard()
 
-# score_value = 0
-# font = pygame.font.Font('freesansbold.ttf', 32)
-# textX = 10
-# textY = 10
-#
-# # game text
-# over_font = pygame.font.Font('freesansbold.ttf', 100)
-# won_font = pygame.font.Font('freesansbold.ttf', 100)
-# replay_font = pygame.font.Font('freesansbold.ttf', 20)
-
-
-# def show_score(x, y):
-#     score = font.render("Score: " + str(score_value), True, (255, 255, 255))
-#     screen.blit(score, (x, y))
-#
-#
-# def check_score(score_value):
-#     if score_value >= 3:
-#         game_won_text()
-#         return True
-#     else:
-#         return False
-#
-# def check_enemy_pos(enemy):
-#     if enemy.positionY > 200:
-#         enemy.positionY = 2000
-#         game_over_text()
-#         return True
-#     else:
-#         return False
-#
-#
-# def game_won_text():
-#     won_text = won_font.render("YOU WIN", True, (255, 255, 255))
-#     replay_text = replay_font.render("Press 'R' To Play Again", True, (255, 255, 255))
-#     rect1 = won_text.get_rect(center=(int(screen.get_width() / 2), int(screen.get_height() / 2)))
-#     rect2 = replay_text.get_rect(center=(int(screen.get_width() / 2), int(screen.get_height() / 2) + 70))
-#     screen.blit(won_text, rect1)
-#     screen.blit(replay_text, rect2)
-#
-#
-# def game_over_text():
-#     over_text = over_font.render("GAME OVER", True, (255, 255, 255))
-#     replay_text = replay_font.render("Press 'R' To Play Again", True, (255, 255, 255))
-#     rect1 = over_text.get_rect(center=(int(screen.get_width() / 2), int(screen.get_height() / 2)))
-#     rect2 = replay_text.get_rect(center=(int(screen.get_width() / 2), int(screen.get_height() / 2) + 70))
-#     screen.blit(over_text, rect1)
-#     screen.blit(replay_text, rect2)
-
-
 # game loop / makes sure everything appears
 running = True
 game_ended = False
